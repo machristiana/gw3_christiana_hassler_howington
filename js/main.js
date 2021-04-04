66593391794d2fe7a349c551066456d432935d76
Becky Hassler <rebeccahassler@ufl.edu>
2021-04-04 18:53:28 -0400

    styled image hover effect color

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+document.getElementById("overlay").addEventListener("mouseover", mouseOver);
+document.getElementById("overlay").addEventListener("mouseout", mouseOut);
 
+function mouseOver() {
+  document.getElementById("ladies-gents-link").style.visibility = "visible";
+}
+
+function mouseOut() {
+  document.getElementById("ladies-gents-link").style.visibility = "hidden";
+}
