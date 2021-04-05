4bcefe6a036f13ce7bdc9320de7dd3ac926d2c27
Becky Hassler <rebeccahassler@ufl.edu>
2021-04-05 19:47:26 -0400

    created html, css, & js for ladies image hover effect

---
+++ js/ladies.js
@@ -0,0 +1,113 @@
+// LADIES 1 - OVERLAY
+//Make text appear over image
+document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverQuoteLadies1);
+document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutQuoteLadies1);
+
+function mouseOverQuoteLadies1() {
+  document.getElementById("quote-ladies1").style.opacity = "1";
+}
+
+function mouseOutQuoteLadies1() {
+  document.getElementById("quote-ladies1").style.opacity = "0";
+}
+
+
+//Make color overlay appear over image
+document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverOverlayLadies1);
+document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutOverlayLadies1);
+
+function mouseOverOverlayLadies1() {
+  document.getElementById("overlay-ladies1").style.opacity = "1";
+}
+
+function mouseOutOverlayLadies1() {
+  document.getElementById("overlay-ladies1").style.opacity = "0";
+}
+
+
+
+
+
+// LADIES 2 - OVERLAY
+//Make text appear over image
+document.getElementById("overlay-container-ladies2").addEventListener("mouseover", mouseOverQuoteLadies2);
+document.getElementById("overlay-container-ladies2").addEventListener("mouseout", mouseOutQuoteLadies2);
+
+function mouseOverQuoteLadies2() {
+  document.getElementById("quote-ladies2").style.opacity = "1";
+}
+
+function mouseOutQuoteLadies2() {
+  document.getElementById("quote-ladies2").style.opacity = "0";
+}
+
+
+//Make color overlay appear over image
+document.getElementById("overlay-container-ladies2").addEventListener("mouseover", mouseOverOverlayLadies2);
+document.getElementById("overlay-container-ladies2").addEventListener("mouseout", mouseOutOverlayLadies2);
+
+function mouseOverOverlayLadies2() {
+  document.getElementById("overlay-ladies2").style.opacity = "1";
+}
+
+function mouseOutOverlayLadies2() {
+  document.getElementById("overlay-ladies2").style.opacity = "0";
+}
+
+
+
+
+// LADIES 3 - OVERLAY
+//Make text appear over image
+document.getElementById("overlay-container-ladies3").addEventListener("mouseover", mouseOverQuoteLadies3);
+document.getElementById("overlay-container-ladies3").addEventListener("mouseout", mouseOutQuoteLadies3);
+
+function mouseOverQuoteLadies3() {
+  document.getElementById("quote-ladies3").style.opacity = "1";
+}
+
+function mouseOutQuoteLadies3() {
+  document.getElementById("quote-ladies3").style.opacity = "0";
+}
+
+
+//Make color overlay appear over image
+document.getElementById("overlay-container-ladies3").addEventListener("mouseover", mouseOverOverlayLadies3);
+document.getElementById("overlay-container-ladies3").addEventListener("mouseout", mouseOutOverlayLadies3);
+
+function mouseOverOverlayLadies3() {
+  document.getElementById("overlay-ladies3").style.opacity = "1";
+}
+
+function mouseOutOverlayLadies3() {
+  document.getElementById("overlay-ladies3").style.opacity = "0";
+}
+
+
+
+
+// LADIES 4 - OVERLAY
+//Make text appear over image
+document.getElementById("overlay-container-ladies4").addEventListener("mouseover", mouseOverQuoteLadies4);
+document.getElementById("overlay-container-ladies4").addEventListener("mouseout", mouseOutQuoteLadies4);
+
+function mouseOverQuoteLadies4() {
+  document.getElementById("quote-ladies4").style.opacity = "1";
+}
+
+function mouseOutQuoteLadies4() {
+  document.getElementById("quote-ladies4").style.opacity = "0";
+}
+
+
+//Make color overlay appear over image
+document.getElementById("overlay-container-ladies4").addEventListener("mouseover", mouseOverOverlayLadies4);
+document.getElementById("overlay-container-ladies4").addEventListener("mouseout", mouseOutOverlayLadies4);
+
+function mouseOverOverlayLadies4() {
+  document.getElementById("overlay-ladies4").style.opacity = "1";
+}
+
+function mouseOutOverlayLadies4() {
+  document.getElementById("overlay-ladies4").style.opacity = "0";
+}
--- js/main.js
+++ js/main.js
@@ -59,28 +59,28 @@ function mouseOutOverlayHome2() {
 
 
 
-// LADIES 1 - OVERLAY
-//Make text appear over image
-document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverQuoteLadies1);
-document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutQuoteLadies1);
-
-function mouseOverQuoteLadies1() {
-  document.getElementById("quote-ladies1").style.opacity = "1";
-}
-
-function mouseOutQuoteLadies1() {
-  document.getElementById("quote-ladies1").style.opacity = "0";
-}
-
-
-//Make color overlay appear over image
-document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverOverlayLadies1);
-document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutOverlayLadies1);
-
-function mouseOverOverlayLadies1() {
-  document.getElementById("overlay-ladies1").style.opacity = "1";
-}
-
-function mouseOutOverlayLadies1() {
-  document.getElementById("overlay-ladies1").style.opacity = "0";
-}
+// // LADIES 1 - OVERLAY
+// //Make text appear over image
+// document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverQuoteLadies1);
+// document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutQuoteLadies1);
+//
+// function mouseOverQuoteLadies1() {
+//   document.getElementById("quote-ladies1").style.opacity = "1";
+// }
+//
+// function mouseOutQuoteLadies1() {
+//   document.getElementById("quote-ladies1").style.opacity = "0";
+// }
+//
+//
+// //Make color overlay appear over image
+// document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverOverlayLadies1);
+// document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutOverlayLadies1);
+//
+// function mouseOverOverlayLadies1() {
+//   document.getElementById("overlay-ladies1").style.opacity = "1";
+// }
+//
+// function mouseOutOverlayLadies1() {
+//   document.getElementById("overlay-ladies1").style.opacity = "0";
+// }
