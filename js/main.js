9ae086787b1060653b29d32438d7c85531387cb1
Becky Hassler <rebeccahassler@ufl.edu>
2021-04-06 18:22:59 -0400

    cleaned up unneeded code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // HOMEPAGE 1 - OVERLAY
 //Make link appear over image
-document.getElementById("overlay-container-home1").addEventListener("mouseover", mouseOverLinkHome1);
-document.getElementById("overlay-container-home1").addEventListener("mouseout", mouseOutLinkHome1);
+document.getElementById("overlay-container-home1").addEventListener("mouseover", mouseOverQuoteHome1);
+document.getElementById("overlay-container-home1").addEventListener("mouseout", mouseOutQuoteHome1);
 
 function mouseOverLinkHome1() {
   document.getElementById("ladies-gents-link-home1").style.opacity = "1";
@@ -52,35 +52,3 @@ function mouseOverOverlayHome2() {
 function mouseOutOverlayHome2() {
   document.getElementById("overlay-home2").style.opacity = "0";
 };
-
-
-
-
-
-
-
-// // LADIES 1 - OVERLAY
-// //Make text appear over image
-// document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverQuoteLadies1);
-// document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutQuoteLadies1);
-//
-// function mouseOverQuoteLadies1() {
-//   document.getElementById("quote-ladies1").style.opacity = "1";
-// }
-//
-// function mouseOutQuoteLadies1() {
-//   document.getElementById("quote-ladies1").style.opacity = "0";
-// }
-//
-//
-// //Make color overlay appear over image
-// document.getElementById("overlay-container-ladies1").addEventListener("mouseover", mouseOverOverlayLadies1);
-// document.getElementById("overlay-container-ladies1").addEventListener("mouseout", mouseOutOverlayLadies1);
-//
-// function mouseOverOverlayLadies1() {
-//   document.getElementById("overlay-ladies1").style.opacity = "1";
-// }
-//
-// function mouseOutOverlayLadies1() {
-//   document.getElementById("overlay-ladies1").style.opacity = "0";
-// }
